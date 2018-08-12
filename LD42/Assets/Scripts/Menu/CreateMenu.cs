21af37b73ab60a720c191d57612ac5ca4790b838
Unknown <smoatwork@gmail.com>
2018-08-12 12:31:11 +1200

    menu and wheel anim

---
+++ LD42/Assets/Scripts/Menu/CreateMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateMenu : MonoBehaviour {
+
+	public enum ButtonTypes
+	{
+		Single,
+		Left_Right
+	}
+
+	[System.Serializable]
+	public struct MenuButton
+	{
+		public string name;
+		public ButtonTypes type;
+	}
+	public List<MenuButton> buttonsList;
+	// Use this for initialization
+	void Start () {
+		float menustart = 0.5f - (buttonsList.Count * 0.5f);
+	}
+}
