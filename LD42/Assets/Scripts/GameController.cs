020b4fed3058d92084009100bce4e523fe497074
Sean michael Mcgifford <845870929@wellington.natcoll.ac.nz>
2018-08-12 17:35:58 +1200

    Add Blight spreading
    Bug where the new blight has the incorrect colour.

--- LD42/Assets/Scripts/GameController.cs
+++ LD42/Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour {
     [SerializeField] Transform worldTransform;
     const int WIDTH = 18;
     const int HEIGHT = 10;
-    Entity[,] board = new Entity[HEIGHT, WIDTH];
+    Entity[,] gameBoard = new Entity[HEIGHT, WIDTH];
     #endregion
 
     #region Player
@@ -25,10 +25,16 @@ public class GameController : MonoBehaviour {
     #endregion
 
     #region Game Control
-    [Tooltip("Time in seconds between move steps by the players")] [SerializeField] float moveTick;
+    [Tooltip("Time in seconds between move steps by the players")]
+    [SerializeField] float moveTick;
     private WaitForSeconds moveWaitTime;
-    [Tooltip("Time in seconds between blight spread events")] [SerializeField] float blightSpreadTick;
+
+    [Tooltip("Time in seconds between blight spread events")]
+    [SerializeField] float blightSpreadTick;
     private WaitForSeconds spreadWaitTime;
+
+    [Tooltip("Number of adjacent entities blight will spread to each SpreadTick")]
+    [SerializeField] int blightSpreadRate = 1;
     #endregion
 
 
@@ -54,19 +60,23 @@ public class GameController : MonoBehaviour {
         // Spawn the players
         SpawnPlayers();
         StartCoroutine(MoveCycle());
-        //StartCoroutine(SpreadCycle());
+        StartCoroutine(SpreadCycle());
     }
 
     private void GenerateBoard()
     {
+        gameBoard = new Entity[HEIGHT, WIDTH];
         for (int r = 0; r < HEIGHT; r++)
         {
             for (int c = 0; c < WIDTH; c++)
             {
                 GameObject instance = Instantiate(entityPrefab, new Vector3(c, r)+worldTransform.position, Quaternion.identity, worldTransform);
-                board[r, c] = instance.GetComponent<Entity>();
+                Entity entity = instance.GetComponent<Entity>();
+                entity.name = string.Format("{0}_{1},{2}", entity.type, c, r);
+                gameBoard[r, c] = instance.GetComponent<Entity>();
             }
         }
+        SetEntityType(Entities.BLIGHT, playerColours[0], new Vector2Int(2, 0));
     }
 
     /// <summary>
@@ -81,7 +91,7 @@ public class GameController : MonoBehaviour {
             return false;
         }
 
-        Entity nextCell = board[position.y, position.x];
+        Entity nextCell = gameBoard[position.y, position.x];
         // Anything can move onto neutral tiles
         if (nextCell.type == Entities.NEUTRAL)
         {
@@ -108,22 +118,23 @@ public class GameController : MonoBehaviour {
         return false;
     }
 
-    private void SetEntityType(Entities type, Vector2Int position, Colour colour)
+    private void SetEntityType(Entities type, Colour colour, Vector2Int position)
     {
-        board[position.y, position.x].type = type;
-        board[position.y, position.x].SetEffectsActive(false);
+        gameBoard[position.y, position.x].type = type;
+        gameBoard[position.y, position.x].name = string.Format("{0}_{1},{2}", type, position.x, position.y);
+        gameBoard[position.y, position.x].SetEffectsActive(false);
         switch (type)
         {
             case Entities.NEUTRAL:
-                board[position.y, position.x].SetSprite(neutralColour.trail);
+                gameBoard[position.y, position.x].SetSprite(neutralColour.trail);
                 break;
             case Entities.PLAYER:
             case Entities.TRAIL:
-                board[position.y, position.x].SetSprite(colour.trail);
+                gameBoard[position.y, position.x].SetSprite(colour.trail);
                 break;
             case Entities.BLIGHT:
-                board[position.y, position.x].SetSprite(colour.blight);
-                board[position.y, position.x].SetEffectsActive(true);
+                gameBoard[position.y, position.x].SetSprite(colour.blight);
+                gameBoard[position.y, position.x].SetEffectsActive(true);
                 break;
             default:
                 Debug.LogWarning("Attempting to set unknown entity type: " + type);
@@ -145,10 +156,8 @@ public class GameController : MonoBehaviour {
                     // move the player
                     player.MoveStep();
 
-                    // set the old position to a trail
-                    SetEntityType(Entities.TRAIL, oldPos, player.colour);
-
-                    board[newPos.y, newPos.x].type = Entities.PLAYER;
+                    SetEntityType(Entities.TRAIL, player.colour, oldPos);                    
+                    gameBoard[newPos.y, newPos.x].type = Entities.PLAYER;
                 }
             }
 
@@ -158,18 +167,122 @@ public class GameController : MonoBehaviour {
 
     IEnumerator SpreadCycle()
     {
-        /*
         while (true) // TODO change this to playing game state or something
         {
-            Iterate over the board
-            if the tile is BLIGHT:
-                 add all available adjacent spaces to list
-                pick X spaces from the list and spread to there
+            yield return spreadWaitTime;
+
+            Dictionary<Vector2Int, Entity> spreadPositions = new Dictionary<Vector2Int, Entity>();
+            // Iterate over the gameBoard
+            for (int r = 0; r < HEIGHT; r++)
+            {
+                for (int c = 0; c < WIDTH; c++)
+                {
+                    // Search for all blight entities
+                    Entity entity = gameBoard[r, c];
+                    if (entity.type == Entities.BLIGHT)
+                    {
+                        // Spread to valid entities
+                        List<Vector2Int> validEntities = GetValidAdjacent(entity, new Vector2Int(c, r));
+                        SpreadToEntities(spreadPositions, entity, validEntities);
+                    }
+                }
+            }
+            ApplySpread(spreadPositions);
+            
         }
-         */
-        yield return moveWaitTime;
     }
 
+    /// <summary>
+    /// Sets the entities at all the stored positions in the given dictionary.
+    /// </summary>
+    /// <param name="spreadPositions"></param>
+    private void ApplySpread(Dictionary<Vector2Int, Entity> spreadPositions)
+    {
+        foreach (Vector2Int pos in spreadPositions.Keys)
+        {
+            Entity entity = spreadPositions[pos];
+            SetEntityType(entity.type, entity.colour, pos);
+        }
+    }
+
+    /// <summary>
+    /// Checks all the valid spaces adjacent to the given position on the gameBoard.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    private List<Vector2Int> GetValidAdjacent(Entity entity, Vector2Int position)
+    {
+        List<Vector2Int> adjacencyList = new List<Vector2Int>();
+        int startX = -1;
+        int startY = -1;
+        int endX = 1;
+        int endY = 1;
+
+        // Edge Checking
+        if (position.x == 0)
+            startX = 0;
+        else if (position.x == WIDTH - 1)
+            endX = 0;
+
+        if (position.y == 0)
+            startY = 0;
+        else if (position.y == HEIGHT - 1)
+            endY = 0;
+
+        // Count adjacent in square around position
+        for (int r = startY; r <= endY; r++)
+        {
+            for (int c = startX; c <= endX ; c++)
+            {
+                Vector2Int adjPos = new Vector2Int(c, r);
+                if (adjPos == Vector2Int.zero) // ignore self
+                    continue;
+
+                if (CheckAhead(entity, position+adjPos))
+                {
+                    adjacencyList.Add(position+adjPos);
+                }
+            }
+        }
+        
+        return adjacencyList;
+    }
+
+    /// <summary>
+    /// Randomly selects a valid position to spread to number is set by the blightSpreadRate
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="positions"></param>
+    private void SpreadToEntities(Dictionary<Vector2Int, Entity> spreadPositions, Entity entity, List<Vector2Int> validPositions)
+    {
+        for (int i = 0; i < blightSpreadRate; i++)
+        {
+            if (i >= validPositions.Count)
+                return;
+
+            int index = Random.Range(0, validPositions.Count);
+            print(string.Format("{0} / {1}", index, validPositions.Count));
+            // If there is a conflict between two entities spreading to the same location,
+            // randomly pick one to override the other
+            if (spreadPositions.ContainsKey(validPositions[index]))
+            {  
+                float roll = Random.Range(0f, 1f);
+                if (roll > 0.5f)
+                {
+                    spreadPositions[validPositions[index]] = entity;
+                }
+            }
+            else
+            {
+                spreadPositions.Add(validPositions[index], entity);
+            }
+            validPositions.RemoveAt(index);
+        }
+    }
+
+    /// <summary>
+    /// Spawns all the players in the game.
+    /// </summary>
     private void SpawnPlayers()
     {
         for (int i = 0; i < numberOfPlayers; i++)
@@ -181,7 +294,7 @@ public class GameController : MonoBehaviour {
             playerScript.SetPlayerNumber(i);
             playersList.Add(playerScript);
 
-            board[(int)spawnPoints[i].localPosition.y, (int)spawnPoints[i].localPosition.x].type = Entities.PLAYER;
+            gameBoard[(int)spawnPoints[i].localPosition.y, (int)spawnPoints[i].localPosition.x].type = Entities.PLAYER;
         }
     }
 	
